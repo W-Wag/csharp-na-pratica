9473e98012489c0c330975341dc71cdaf63ce0f0
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2024-03-13 20:58:20 -0300

    Iniciando o projeto

---
+++ WebStorage.Api/Controllers/storageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+    namespace WebStorage.Api.Controllers;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class storageController : ControllerBase
+    {
+    [HttpPost]
+    public IActionResult uploadImage(IFormFile file)
+    {
+        return Created();
+    }
+    }
+
